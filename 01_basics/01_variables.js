d69ba7c3b68025a4a5f2be59076cc24f52bf25a6
Kajal pandey <129525113+Prachi7905@users.noreply.github.com>
2025-07-22 12:15:01 +0000

    changes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID =1235
+let accountEmail="Kajal@gmail.com"
+var accountPassword = "12546"
+accountCity = "jaipur"
+let accountState;
+
+//accountID =4
+accountEmail="hcsjjb@gmail.com"
+accountPassword = "19"
+accountCity = "Ayodhya"
+
+//console.log(accountID)
+
+console.table([accountEmail,accountPassword,accountCity, accountState]);
