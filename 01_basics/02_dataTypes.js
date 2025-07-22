252f2dddcb912eff61174c1f1702709c2d32cc10
Kajal pandey <129525113+Prachi7905@users.noreply.github.com>
2025-07-22 13:06:50 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+//alert(3+4) //we are using node js not browser
+
+console.log(3+3); 
+console.log("Kajal"); // code readability should be high
+
+let name= "Kajal"
+let age =18
+let isLoggedIn= false
+let state;
+//number
+//bigInt
+//Boolean- true/false
+//null- standalone value
+//undefined
+//symbol- unique
+//object
+
+console.log(typeof "Kajal"); //datatype- string
+console.log(typeof age); //datatype- number
+console.log(typeof isLoggedIn); //datatype- boolean
+console.log(typeof null);  //datatpe- object (why?)
+console.log(typeof undefined); //dataype- undefined 
+
+
+
