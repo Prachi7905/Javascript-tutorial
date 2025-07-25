9c37051c8f4a5cc3b7757669fd23ce5698ea4677
Kajal pandey <129525113+Prachi7905@users.noreply.github.com>
2025-07-25 10:51:57 +0000

    changes done

---
+++ 01_basics/04_camparison.js
@@ -0,0 +1,21 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+//console.log("2">1);
+//console.log("02">1);
+
+console.log(null>0);  //comparison operator changes null into 0 that why null>=0 is true
+console.log(null==0);  //equality operator
+console.log(null>=0);  //compare operator
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// === also checks data type and then compares
+
+console.log("2" === 2);
+
