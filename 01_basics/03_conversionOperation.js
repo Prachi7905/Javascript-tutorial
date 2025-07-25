1e1e1a0c5dc89dd141dc38205333431380f3a518
Kajal pandey <129525113+Prachi7905@users.noreply.github.com>
2025-07-25 09:49:53 +0000

    datatype conversion done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -56,3 +56,38 @@ let someNumber = 33
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//********************************** Operations **********************************************
+
+let value =3
+let negValue = -value;
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2**2); // Exponentiation
+//console.log(2%2); // Modulus
+
+let str1= "hello"
+let str2= " Prachi"
+let str3 = str1 + str2
+console.log(str3);
+ 
+//console.log (1 + "2");
+//console.log("1" + 2 );
+//console.log("1"+ 2 + 2);
+//console.log(1 + 2 + "2");
+//console.log((1+2) * 9 % 2); //use parenthesis to control order of operations 
+
+console.log(+true);
+//console.log(true+);   //this will give an error
+let num1, num2, num3
+num1=num2=num3= 2+2
+
+let gameCounter =100
+++gameCounter; // Increment operator
+console.log(gameCounter);
+
+
