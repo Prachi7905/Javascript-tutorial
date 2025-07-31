e74be14126323f586de88e21909e557b28622dd0
Kajal pandey <129525113+Prachi7905@users.noreply.github.com>
2025-07-31 12:20:32 +0000

    done

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -2,4 +2,5 @@ const myBooks = ["Hindi", "english", "math"]
 const myHeroes = ["Thor", "ironman", "hulk"]
 
 myBooks.push(myHeroes)
-console.log(myBooks);
+console.log(myBooks);  //it will consider second array as a 4th value of array
+console.log(myBooks[3][1]);   //ironman
