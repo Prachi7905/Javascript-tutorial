9eadbf26fd32eff7f2c4f373dc2a408019369bb2
Kajal pandey <129525113+Prachi7905@users.noreply.github.com>
2025-08-03 10:05:53 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,9 +2,12 @@
 
 //object literals
 
+const mySym = Symbol("key1")   //symbol
+
 const JsUser = {
     name : "Kajal",
     "Full name" : "Kajal Pandey",
+    [mySym] : "mykey1",
     age: 20,
     location : "jaipur",
     email : "ksgy@gmail.com",
@@ -15,3 +18,20 @@ const JsUser = {
 console.log(JsUser.email)  //this is not a good method to access the element because in some cases i.e Full name, we won't be able to access that element
 console.log(JsUser["email"])  // ksgy@gmail.com
 console.log(JsUser["Full name"]) //Kajal Pandey
+console.log(JsUser[mySym])
+
+JsUser.email = "hsbu@gmail.com" //change the values
+//Object.freeze(JsUser)  //no one can change value after freeze
+// JsUser.email = "hbdjka@gmail.com"
+console.log(JsUser);
+
+JsUser.greeting = function(){                  //creating a function
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){                  //creating a function
+    console.log(`Hello JS user, ${this.name} `);
+}
+
+//console.log(JsUser.greeting); // [Function (anonymous)]
+console.log(JsUser.greeting()); // Hello JS user
+console.log(JsUser.greetingTwo());
