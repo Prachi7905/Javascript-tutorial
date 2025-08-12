e143ac7fcbe44da3cd04cd076a3eaa00563538c9
Kajal pandey <129525113+Prachi7905@users.noreply.github.com>
2025-08-12 17:05:31 +0000

    IIFE done

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,31 @@
+// Immediately Invoked Function Expression
+
+// function chai(){
+//     console.log('DB CONNECTED')
+// }
+// chai()                       // DB CONNECTED
+
+(function chai(){
+    console.log(`DB CONNECTED`)
+})();  //  To write two or multiple IIFE one should be end with semicolon ;
+
+// ()()  
+// IIFE is a function which immediately executes after it's creation . 
+// It is wriiten as one parentheses for wrapping the function and other for execution ()();
+
+( function aurcode (){     // This is named IIFE
+console.log(`DB CONNECTED TWO`)
+
+} )();
+
+( ()=> {
+console.log(`DB CONNECTED THREE`)
+
+} )();
+
+
+// for variables
+( (name)=> {            // This is simple IIFE
+console.log(`DB CONNECTED FOUR  ${name}`)
+
+} )('Kajal')
