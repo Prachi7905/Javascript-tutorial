f79fc18e737aa13c571286717021672d7fdf4906
Kajal pandey <129525113+Prachi7905@users.noreply.github.com>
2025-08-18 07:52:26 +0000

    Reduce

--- 05_loops/08_eight.js
+++ 05_loops/08_eight.js
@@ -1,1 +1,36 @@
 // Reduce
+
+const myNum = [1,2,3,4]
+
+// const myTotal = myNum.reduce(function(acc, cvalue){
+//     console.log(`acc : ${acc} and cvalue : ${cvalue}`);
+    
+//     return acc + cvalue
+// }, 0)
+
+//console.log(myTotal);  // 10
+
+const myTotal = myNum.reduce( (acc, curr) => acc +curr, 0)
+//console.log(myTotal);  // 10
+
+const shoppingCart = [
+    {
+        itemName : "js course",
+        price : 2999
+    },
+    {
+        itemName : "mobile dev course",
+        price : 5999
+    },
+    {
+        itemName : "data science course",
+        price : 7999
+    },
+    {
+        itemName : "py course",
+        price : 1999
+    },
+]
+const priceToPay = shoppingCart.reduce((acc , item) => acc + item.price, 0)
+console.log(priceToPay);  // 18996
+
