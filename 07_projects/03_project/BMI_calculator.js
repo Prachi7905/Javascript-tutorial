ed3bb7c8ced03f126de8771d175ff55c890711e1
Kajal pandey <129525113+Prachi7905@users.noreply.github.com>
2025-08-25 07:16:15 +0000

    BMI calculator

--- 07_projects/03_project/BMI_calculator.js
+++ 07_projects/03_project/BMI_calculator.js
@@ -1,22 +1,3 @@
-// const form = document.querySelector('form')
-
-// //this usecase will give you empty
-// // const weight= parseInt(document.querySelector('#weight').value)
-
-// form.addEventListener('submit', function(e){
-//     e.preventDefault();
-
-//     const height= parseInt(document.querySelector('#height').value);
-//     const weight= parseInt(document.querySelector('#weight').value);
-//     const results= document.querySelector('#results');
-
-// if(height ===''|| height<0 || isNaN(height)){
-//   results.innerHTML= `Please give a valid height ${height}`;
-
-// }
-
-// });
-
 const form = document.querySelector('form');
 // this usecase will give you empty
 // const height = parseInt(document.querySelector('#height').value)
@@ -35,7 +16,14 @@ form.addEventListener('submit', function (e) {
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
+   // results.innerHTML = `<span>${bmi}</span>`;
+    if(bmi<18.6){
+        results.innerHTML=`<span>Under weigth :${bmi}</span>`;
+        }else if(bmi>18.6 && bmi<24.9){
+            results.textContent=`Normal Weight:${bmi}`;
+        }else{
+            results.textContent=`Over Weight : ${bmi}`
+        }
   }
 });
 
