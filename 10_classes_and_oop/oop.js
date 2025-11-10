ff3522e56c43944eee63543de3a20c8f5becfd1e
Kajal pandey <129525113+Prachi7905@users.noreply.github.com>
2025-11-10 18:13:18 +0000

    oops

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -4,11 +4,37 @@ const user = {
     signedIn : true,
 
     getUserDetails: function(){
-        console.log("Got user details from DB");
+        //console.log("Got user details from DB");
+        //console.log(`Username : ${this.username}`);
+        console.log(this);
         
     }
 }
 
-console.log(user.username);
-console.log(user.getUserDetails());
+//console.log(user.username);
+//console.log(user.getUserDetails());
+//console.log(this);
+
+// const promiseOne = new Promise()
+// const date = new Date()
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+        
+    }
+
+    return this
+}
+
+const userOne = new User("Kajal", 12, true)
+const userTwo = new User("Pandey",11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
+
+
 
