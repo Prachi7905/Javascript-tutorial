f5fe2c31d4b790d20be0f3f7bfd552b60bec810a
Kajal pandey <129525113+Prachi7905@users.noreply.github.com>
2025-11-13 18:33:14 +0000

    objects

--- 10_classes_and_oop/objects.js
+++ 10_classes_and_oop/objects.js
@@ -10,4 +10,17 @@ console.log(multipleBy5.prototype);
 
 function createUser (username, score){
     this.username= username
+    this.score = score
 }
+createUser.prototype.increment = function () {
+    this.score++
+}
+createUser.prototype.printMe=function(){
+    console.log(`price is ${this.score}`);
+    
+}
+
+const chai = new createUser("chai", 25)
+const tea = createUser("tea", 250)
+
+chai.printMe()
